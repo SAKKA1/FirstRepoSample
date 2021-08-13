4eae71373629cf7c0a63fa55dcd1548664f6e26c
Mahmoud Elsaka <mahmoudelsakka119@gmail.com>
2021-08-13 19:41:39 +0200

    change js

--- js/js.js
+++ js/js.js
@@ -10,3 +10,17 @@ const navSlide = () => {
    
 }
 navSlide();
+
+
+const navSlide = () => {
+    const humburger = document.querySelector(".humburger");
+    const nav = document.querySelector("ul");
+
+    humburger.addEventListener('click' ,() =>{
+        nav.classList.toggle('ul-active');
+        humburger.classList.toggle('burger');
+
+    });
+   
+}
+navSlide();
