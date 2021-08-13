8d6332c03b21071a12ea6e01328b976e90936f29
Mahmoud Elsaka <mahmoudelsakka119@gmail.com>
2021-08-13 19:40:01 +0200

    first commit

---
+++ js/js.js
@@ -0,0 +1,12 @@
+const navSlide = () => {
+    const humburger = document.querySelector(".humburger");
+    const nav = document.querySelector("ul");
+
+    humburger.addEventListener('click' ,() =>{
+        nav.classList.toggle('ul-active');
+        humburger.classList.toggle('burger');
+
+    });
+   
+}
+navSlide();
